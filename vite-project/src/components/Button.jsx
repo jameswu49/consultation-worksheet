a8cd31e912de9d4043dc6c4b7e825d098bac313d
Jimmy Wu <jameswu49@gmail.com>
2023-07-01 20:05:07 -0700

    updated text

--- vite-project/src/components/Button.jsx
+++ vite-project/src/components/Button.jsx
@@ -3,9 +3,9 @@ export default function Button() {
         <>
             <div>
                 <div className="flex justify-center border-red-700 border-4 rounded-md bg-red-700">
-                    <button className="border-black border-2 w-full py-2 bg-red-500 text-white rounded-md" type="submit">SUBMIT FORM</button>
+                    <button className="border-black border-2 w-full py-2 text-white rounded-md gradient" type="submit">SUBMIT FORM</button>
                 </div>
-                <p className="text-center font-bold mt-10 mb-20">All information submitted on this form will  be kept strictly confidential.</p>
+                <p className="text-center font-bold mt-10 mb-20 text-sm">All information submitted on this form will  be kept strictly confidential.</p>
             </div>
         </>
     )
